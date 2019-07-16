f93a689d829a78dc3b0ece61a53e784308d8f43f
Will Payne <Will.Payne@softwire.com>
2019-07-16 15:05:29 +0100

    Logging added and lots of validation included

--- SupportBank/index.js
+++ SupportBank/index.js
@@ -1,57 +1,101 @@
-const readlineSync = require('readline-sync');
+const log4js = require('log4js')
 const csv = require('csv-streamify');
 const fs = require('fs');
 const moment = require('moment')
 const users = [];
+const invalidTransactions = [];
+let lineNum = 0;
+
+log4js.configure({
+    appenders: {
+        file: { type: 'fileSync', filename: 'debug.log' }
+    },
+    categories: {
+        default: { appenders: ['file'], level: 'trace'}
+    }
+});
+
+const logger = log4js.getLogger('file');
 
 const parser = csv({ objectMode : true }, (err, result) => {
     if(err){
         console.error(err);
+        logger.error(err)
         return;
     }
     result.shift();
     result.forEach(parseCsvLine);
     if(process.argv[2] === "All"){
+        logger.trace("Finding all users")
         findAll();
     }else if(process.argv[2] === undefined){
+        logger.info("No command string given exiting")
         console.log("Give a username as an argument or \"All\"");
     }else {
+        logger.trace("Getting specific user info")
         let u = User.get(process.argv[2])
         if(u === undefined){
+            logger.error("Not valid user exiting")
             console.log("No such user");
         }else{
+            logger.trace("Valid user, getting complete summary")
             u.getCompleteSummary();
         }
     }
+    if(invalidTransactions.length > 0){
+        console.log("Finished program with the invalid lines ")
+        invalidTransactions.forEach(t => {
+            console.log(t);
+        })
+        console.log("These lines were ommited");
+    }
 })
+
+fs.createReadStream('./DodgyTransactions2015.csv').pipe(parser);
 
 function findAll(){
     users.forEach(u => {
+        logger.trace("Getting quick summary for " + u.username)
         u.getQuickSummary();
     });
 }
 
 function parseCsvLine(line){
+    lineNum++;
+    logger.trace("Passing csv line " + lineNum)
     transaction = Transaction.createTransactionFromLine(line);
-    to = User.getOrMake(transaction.to);
-    from = User.getOrMake(transaction.from);
-    to.addToTransaction(transaction);
-    from.addFromTransaction(transaction);
+    if(transaction.valid){
+        to = User.getOrMake(transaction.to);
+        from = User.getOrMake(transaction.from);
+        to.addToTransaction(transaction);
+        from.addFromTransaction(transaction);
+    }else{
+        invalidTransactions.push("Line: " + lineNum + ", Data: " + line);
+    }
 }
-
-
-fs.createReadStream('./Transactions2014.csv').pipe(parser);
 
 class Transaction {
     constructor(date, to, from, reason, amount){
+        this.valid = true;
+        logger.info("Parsing date " + date);
         this.date = moment(date, "DD/MM/YYYY");
+        if(!this.date.isValid()){ 
+            this.valid = false;
+            logger.error("Invalid date")
+        }
         this.to = to;
         this.from = from;
         this.reason = reason;
         this.amount = parseFloat(amount);
+        logger.info("Parsing amount " + amount + " to " + this.amount);
+        if(!this.amount && this.amount !== 0) {
+            this.valid=false;
+            logger.error("Cannot parse amount")
+        };
     }
 
     static createTransactionFromLine(line){
+        if(line.length !== 5) logger.error("Line has " + line.length + " elements insead of 5");
         return new Transaction(line[0], line[1], line[2], line[3], line[4]);
     }
 }
@@ -98,6 +142,7 @@ class User {
     static getOrMake(name){
         let result = users.find((user) => user.username === name);
         if(result === undefined) {
+            logger.info("Creating new user " + name);
             result = new User(name)
             users.push(result)
         }
